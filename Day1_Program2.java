865dbd8ddf95fecdfbd6f2d4c7a8c6628082dbeb
vaishnavi-senthilkumar <111259303+vaishnavi-senthilkumar@users.noreply.github.com>
2025-03-07 11:44:04 +0530

    Create Day1_Program2.java

---
+++ Day1_Program2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Day1_Program2 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		char ch=s.next().charAt(0);
+		System.out.println((float)a);
+		System.out.println((int)ch);
+		System.out.println((char)a);
+
+	}
+
+}
